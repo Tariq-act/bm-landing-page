f8c789845e0d3869f52b64edd30e1067d84aca3f
Tariq-act <khantarikact@gmail.com>
2024-05-19 13:31:28 +0530

    color issue

--- src/components/OurServices/ServicesTabs.tsx
+++ src/components/OurServices/ServicesTabs.tsx
@@ -8,7 +8,7 @@ const ServicesTabs = () => {
   const [openTab, setOpenTab] = useState<number>(1);
 
   const activeClasses =
-    'border-l border-t border-r rounded-t-lg text-blue-700 bg-[#86C421] text-white text-base lg:text-lg tracking-widest';
+    'border-l border-t border-r rounded-t-lg text-white bg-[#86C421] text-base lg:text-lg tracking-widest';
   const inactiveClasses = 'text-primary hover:text-blue-700';
 
   const handleTabChange = (tabNumber: number) => {
@@ -17,340 +17,146 @@ const ServicesTabs = () => {
 
   return (
     <div className='p-6 border rounded-md'>
-      <ul className='flex flex-wrap justify-center md:justify-start border-b transition-all ease-in-out '>
-        <li
-          onClick={() => handleTabChange(1)}
-          className={`mr-1 ${openTab === 1 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 1 ? activeClasses : inactiveClasses
+      <ul className='flex flex-wrap justify-center md:justify-start border-b'>
+        {[
+          { id: 1, label: 'Personal Loan' },
+          { id: 2, label: 'Business Loan' },
+          { id: 3, label: 'Working Capital' },
+          { id: 4, label: 'Property Loan' },
+          { id: 5, label: 'Vehicle Loan' },
+          { id: 6, label: 'Education Loan' },
+        ].map((tab) => (
+          <li
+            key={tab.id}
+            onClick={() => handleTabChange(tab.id)}
+            className={`mr-1 cursor-pointer ${
+              openTab === tab.id ? '-mb-px' : ''
             }`}
           >
-            Personal Loan
-          </button>
-        </li>
-        <li
-          onClick={() => handleTabChange(2)}
-          className={`mr-1 ${openTab === 2 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 2 ? activeClasses : inactiveClasses
-            }`}
-          >
-            Business Loan
-          </button>
-        </li>
-        <li
-          onClick={() => handleTabChange(3)}
-          className={`mr-1 ${openTab === 3 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 3 ? activeClasses : inactiveClasses
-            }`}
-          >
-            Working Capital
-          </button>
-        </li>
-        <li
-          onClick={() => handleTabChange(4)}
-          className={`mr-1 ${openTab === 4 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 4 ? activeClasses : inactiveClasses
-            }`}
-          >
-            Property Loan
-          </button>
-        </li>
-        <li
-          onClick={() => handleTabChange(5)}
-          className={`mr-1 ${openTab === 5 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 5 ? activeClasses : inactiveClasses
-            }`}
-          >
-            Vehicle Loan
-          </button>
-        </li>
-        <li
-          onClick={() => handleTabChange(6)}
-          className={`mr-1 ${openTab === 6 ? '-mb-px' : ''}`}
-        >
-          <button
-            className={`bg-white inline-block py-2 px-4 font-semibold ${
-              openTab === 6 ? activeClasses : inactiveClasses
-            }`}
-          >
-            Education Loan
-          </button>
-        </li>
+            <button
+              className={`bg-white inline-block py-2 px-4 font-semibold transition-colors ${
+                openTab === tab.id ? activeClasses : inactiveClasses
+              }`}
+            >
+              {tab.label}
+            </button>
+          </li>
+        ))}
       </ul>
       <div className='w-full mt-4'>
-        <div
-          className={`${
-            openTab === 1 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/personal_loan.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Unlock Financial Freedom: Navigating the World of Personal Loans
-            </h3>
-            <p>
-              Experience financial freedom without collateral at Bank Master.
-              Your ultimate guide to personal loans, navigate low interest
-              rates, terms, and eligibility confidently. Empower your financial
-              journey today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Unsecured Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`21 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
+        {[
+          {
+            id: 1,
+            image: 'images/loans/personal_loan.svg',
+            title:
+              'Unlock Financial Freedom: Navigating the World of Personal Loans',
+            description:
+              'Experience financial freedom without collateral at Bank Master. Your ultimate guide to personal loans, navigate low interest rates, terms, and eligibility confidently. Empower your financial journey today.',
+            features: [
+              'Unsecured Loans',
+              `21 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+          {
+            id: 2,
+            image: 'images/loans/business_loan.svg',
+            title:
+              'Amplify Business Prosperity: Uncover the Power of Business Loans',
+            description:
+              'Seize Financial Advantage at Bank Master, Collateral-Free. Your Ultimate Business Loan Guide. Navigate Interest Rates, Terms, and Eligibility with Confidence. Empower Your Business Journey Today.',
+            features: [
+              'Unsecured Loans',
+              `21 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+          {
+            id: 3,
+            image: 'images/loans/working_capital.svg',
+            title:
+              'Optimize Operations: Navigating the World of Working Capital',
+            description:
+              'Enhance Financial Agility with Bank Master. Your Comprehensive Guide to Working Capital. Navigate Terms, Interest Rates, and Eligibility with Confidence. Empower Your Business Journey Today.',
+            features: [
+              'Secured & Unsecured Loans',
+              `21 Banks & NBFC's Partners`,
+              `6 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+          {
+            id: 4,
+            image: 'images/loans/property_loan.svg',
+            title:
+              'Transform Dreams into Reality: Explore the World of Property & Mortgage Loans',
+            description:
+              'Turn Your Property Dreams into Achievements with Bank Master. Your Inspirational Guide to Property & Mortgage Loans, revealing strategies to transform your aspirations into reality. Navigate Interest Rates, Terms, and Eligibility with Confidence. Empower Your Property Journey Today.',
+            features: [
+              'Purchase & Mortgage Loans',
+              `32 Banks & NBFC's Partners`,
+              `6 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+          {
+            id: 5,
+            image: 'images/loans/vehicle_loan.svg',
+            title:
+              'Drive Your Dreams: Cruising Through the World of Vehicle Loans',
+            description:
+              'Turn Your Automotive Dreams into Reality with Bank Master. Your Ultimate Guide to Vehicle Loans, unveiling strategies to make your dream vehicle a reality. Navigate Interest Rates, Terms, and Eligibility with Confidence, Accelerating Your vehicle Journey Today.',
+            features: [
+              'New & Old Vehicle Loans',
+              `8 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+          {
+            id: 6,
+            image: 'images/loans/education_loan.svg',
+            title:
+              'Level Up Your Skills: Navigating the World of Educational Loans',
+            description:
+              'Transform Your Academic Dreams into Reality with Bank Master. Your Definitive Guide to Educational Loans, revealing strategies to make your educational goals a reality. Navigate Interest Rates, Terms, and Eligibility with Confidence. Empower Your Educational Journey Today.',
+            features: [
+              'New & Old Vehicle Loans',
+              `8 Banks & NBFC's Partners`,
+              'Credit Facilities',
+            ],
+          },
+        ].map((tabContent) => (
+          <div
+            key={tabContent.id}
+            className={`${
+              openTab === tabContent.id ? 'block' : 'hidden'
+            } flex flex-col md:flex-row items-center`}
+          >
+            <div className='flex-1 lg:flex-auto'>
+              <Image
+                src={tabContent.image}
+                width={500}
+                height={500}
+                alt='loan-image'
+                loading='lazy'
+              />
+            </div>
+            <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
+              <h3 className='text-2xl font-bold tracking-wide mb-4'>
+                {tabContent.title}
+              </h3>
+              <p>{tabContent.description}</p>
+              <div className='flex flex-col gap-2'>
+                {tabContent.features.map((feature, index) => (
+                  <div key={index} className='flex items-center gap-2'>
+                    <Check className='text-primary font-bold' />
+                    <p>{feature}</p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-        </div>
-
-        <div
-          className={`${
-            openTab === 2 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/business_loan.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Amplify Business Prosperity: Uncover the Power of Business Loans
-            </h3>
-            <p>
-              Seize Financial Advantage at Bank Master, Collateral-Free. Your
-              Ultimate Business Loan Guide. Navigate Interest Rates, Terms, and
-              Eligibility with Confidence. Empower Your Business Journey Today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Unsecured Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`21 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={`${
-            openTab === 3 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/working_capital.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Optimize Operations: Navigating the World of Working Capital
-            </h3>
-            <p>
-              Enhance Financial Agility with Bank Master. Your Comprehensive
-              Guide to Working Capital. Navigate Terms, Interest Rates, and
-              Eligibility with Confidence. Empower Your Business Journey Today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Secured & Unsecured Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`21 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`6 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={`${
-            openTab === 4 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/property_loan.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Transform Dreams into Reality: Explore the World of Property &
-              Mortgage Loans
-            </h3>
-            <p>
-              Turn Your Property Dreams into Achievements with Bank Master. Your
-              Inspirational Guide to Property & Mortgage Loans, revealing
-              strategies to transform your aspirations into reality. Navigate
-              Interest Rates, Terms, and Eligibility with Confidence. Empower
-              Your Property Journey Today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Purchase & Mortgage Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`32 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`6 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={`${
-            openTab === 5 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/vehicle_loan.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Drive Your Dreams: Cruising Through the World of Vehicle Loans
-            </h3>
-            <p>
-              Turn Your Automotive Dreams into Reality with Bank Master. Your
-              Ultimate Guide to Vehicle Loans, unveiling strategies to make your
-              dream vehicle a reality. Navigate Interest Rates, Terms, and
-              Eligibility with Confidence, Accelerating Your vehicle Journey
-              Today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>New & Old Vehicle Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`8 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={`${
-            openTab === 6 ? 'block' : 'hidden'
-          } flex flex-col md:flex-row items-center`}
-        >
-          <div className='flex-1 lg:flex-auto'>
-            <Image
-              src={'images/loans/education_loan.svg'}
-              width={500}
-              height={500}
-              alt='personal-loan'
-              loading='lazy'
-            />
-          </div>
-          <div className='flex flex-1 lg:flex-auto flex-col gap-3'>
-            <h3 className='text-2xl font-bold tracking-wide mb-4'>
-              Level Up Your Skills: Navigating the World of Educational Loans
-            </h3>
-            <p>
-              Transform Your Academic Dreams into Reality with Bank Master. Your
-              Definitive Guide to Educational Loans, revealing strategies to
-              make your educational goals a reality. Navigate Interest Rates,
-              Terms, and Eligibility with Confidence. Empower Your Educational
-              Journey Today.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>New & Old Vehicle Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>International & Domestic Loans</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>{`8 Banks & NBFC's Partners`}</p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Check className='text-primary font-bold' />
-                <p>Credit Facilities</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
