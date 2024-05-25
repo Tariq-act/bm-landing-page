153e7947cc2ed218f8dbbef636e15903498ce1cc
Tariq-act <khantarikact@gmail.com>
2024-05-25 10:52:02 +0530

    make OurUps component readable

--- src/components/OurUsps/index.tsx
+++ src/components/OurUsps/index.tsx
@@ -1,47 +1,41 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const features = [
+  {
+    name: 'Loan Process',
+    description:
+      'Tired of complex loan processes? Fear not! We simplify the journey for you, ensuring transparency at every step for your peace of mind.',
+    image: '/images/feature/loanProcess.svg',
+  },
+  {
+    name: 'Financial Doctor',
+    description: `Experience Clear Solutions to Your Financial Queries with Our On-Ground Masters. As our Network Grows, Bringing Top-notch Experts from Banks and NBFC's Closer to Solve Your Financial Challenges.`,
+    image: '/images/feature/financialDoctor.svg',
+  },
+  {
+    name: 'My Tracker',
+    description: `Centralize Your Loans at our MYLocker for Timely Payments, reduced Maintenance & Boosting Your Cibil Score.`,
+    image: '/images/feature/tracker.svg',
+  },
+  {
+    name: 'Drive In',
+    description: `Revolutionize your finance game with our app's real-time insights on loans, interest rates, credit cards, and CIBIL matters. Stay confident, stay informed!.`,
+    image: '/images/feature/driveInn.svg',
+  },
+  {
+    name: 'Loan Eligibility tool',
+    description: `Revolutionize your loan selection with our cutting-edge Loan Eligibility Master Tool. Elevate your financial decisions instantly and confidently. Unleash the power of tailored loan options at your fingertips.`,
+    image: '/images/feature/LoanEligibility.svg',
+  },
+];
 
 const OurUsps = () => {
   const [selectedFeature, setSelectedFeature] = useState<number>(0);
   const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
 
-  const images = useMemo(
-    () => [
-      '/images/feature/loanProcess.svg',
-      '/images/feature/financialDoctor.svg',
-      '/images/feature/tracker.svg',
-      '/images/feature/driveInn.svg',
-      '/images/feature/LoanEligibility.svg',
-    ],
-    []
-  );
-  const Info = [
-    {
-      name: 'Loan Process',
-      description:
-        'Tired of complex loan processes? Fear not! We simplify the journey for you, ensuring transparency at every step for your peace of mind.',
-    },
-    {
-      name: 'Financial Doctor',
-      description: `Experience Clear Solutions to Your Financial Queries with Our On-Ground Masters. As our Network Grows, Bringing Top-notch Experts from Banks and NBFC's Closer to Solve Your Financial Challenges.`,
-    },
-    {
-      name: 'My Tracker',
-      description: `Centralize Your Loans at our MYLocker for Timely Payments, reduced Maintenance & Boosting Your Cibil Score.`,
-    },
-    {
-      name: 'Drive In',
-      description: `Revolutionize your finance game with our app's real-time insights on loans, interest rates, credit cards, and CIBIL matters. Stay confident, stay informed!.`,
-    },
-    {
-      name: 'Loan Eligibility tool',
-      description: `Revolutionize your loan selection with our cutting-edge Loan Eligibility Master Tool. Elevate your financial decisions instantly and confidently. Unleash the power of tailored loan options at your fingertips.`,
-    },
-  ];
-
-  // Function to handle click on list items
   const handleFeatureClick = (index: number) => {
     if (index !== selectedFeature) {
       setIsTransitioning(true);
@@ -52,13 +46,12 @@ const OurUsps = () => {
     }
   };
 
-  // Prefetching images to improve performance
   useEffect(() => {
-    images.forEach((image) => {
+    features.forEach((feature) => {
       const img = new window.Image();
-      img.src = image;
+      img.src = feature.image;
     });
-  }, [images]);
+  }, []);
 
   return (
     <div
@@ -81,21 +74,20 @@ const OurUsps = () => {
 
       <div className='bg-primary lg:w-3/5 flex flex-col items-center justify-center md:justify-start gap-5 md:gap-32 lg:gap-32 md:px-10 py-8'>
         <div className='flex items-center justify-center md:justify-start gap-10 md:gap-32 lg:gap-32 px-5 md:px-10'>
-          <div>
-            <ul className='flex flex-col gap-2'>
-              {Info.map((feature, index) => (
-                <li
-                  key={index}
-                  className={`cursor-pointer ${
-                    selectedFeature === index ? 'text-secondary' : ''
-                  }`}
-                  onClick={() => handleFeatureClick(index)}
-                >
-                  {feature.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+          <ul className='flex flex-col gap-2'>
+            {features.map((feature, index) => (
+              <li
+                key={index}
+                className={`cursor-pointer ${
+                  selectedFeature === index ? 'text-secondary' : ''
+                }`}
+                onClick={() => handleFeatureClick(index)}
+              >
+                {feature.name}
+              </li>
+            ))}
+          </ul>
+
           <div className='flex items-center gap-3'>
             <div
               className={`transition-opacity transform duration-500 ${
@@ -103,11 +95,11 @@ const OurUsps = () => {
               }`}
             >
               <Image
-                src={images[selectedFeature]}
+                src={features[selectedFeature].image}
                 width={160}
                 height={160}
                 loading='lazy'
-                alt={Info[selectedFeature].name}
+                alt={features[selectedFeature].name}
               />
             </div>
 
@@ -117,9 +109,9 @@ const OurUsps = () => {
               }`}
             >
               <h1 className='font-semibold md:text-2xl mb-2'>
-                {Info[selectedFeature].name}
+                {features[selectedFeature].name}
               </h1>
-              <p className='text-sm'>{Info[selectedFeature].description}</p>
+              <p className='text-sm'>{features[selectedFeature].description}</p>
             </div>
           </div>
         </div>
@@ -130,9 +122,9 @@ const OurUsps = () => {
           }`}
         >
           <h1 className='font-semibold md:text-2xl mb-2'>
-            {Info[selectedFeature].name}
+            {features[selectedFeature].name}
           </h1>
-          <p className='text-sm'>{Info[selectedFeature].description}</p>
+          <p className='text-sm'>{features[selectedFeature].description}</p>
         </div>
       </div>
     </div>
